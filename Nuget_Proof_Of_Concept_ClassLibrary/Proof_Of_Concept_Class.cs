e7b36aac8749cf42b02f824a66839a3983c843d9
Ricardo Heimoski <rheimoski@wiley.com>
2021-01-07 17:00:32 -0300

    Add project files.

---
+++ Nuget_Proof_Of_Concept_ClassLibrary/Proof_Of_Concept_Class.cs
@@ -0,0 +1,15 @@
+﻿namespace Nuget_Proof_Of_Concept_ClassLibrary
+{
+    public class Proof_Of_Concept_Class
+    {
+        public Proof_Of_Concept_Class()
+        {
+
+        }
+
+        public static string HelloWorld()
+        {
+            return "Hello World";
+        }
+    }
+}
